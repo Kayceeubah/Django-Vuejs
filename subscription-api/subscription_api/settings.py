39b9f9da8d692784bd2d36e88568aa6304c2c829
Victor <vsifah@gmail.com>
2022-07-23 13:25:12 +0100

    deploying a django-vuejs app

--- subscription-api/subscription_api/settings.py
+++ subscription-api/subscription_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=2ubex9f$sjwq80g1w6hr*(u23sb1z%@#51)u+3--$8cnqhmof'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+
 
 CORS_ORIGIN_ALLOW_ALL = True
